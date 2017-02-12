1ef33ba3a365de9d77d9f52317eba5367bc3a6e5
Joy <joywen0520@gmail.com>
2017-02-12 17:07:01 +0800

    [MovieViewer] Empty activity

---
+++ app/src/main/java/movieviewer/joy/com/movieviewer/MainActivity.java
@@ -0,0 +1,13 @@
+package movieviewer.joy.com.movieviewer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
